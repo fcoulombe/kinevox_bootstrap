6a38063e278c9c2b1392a22574317544095b8a1d
fcoulombe <fcoulombe@silentfalls.org>
2013-04-23 13:56:45 -0300

    franky ports the clone script to linux

---
+++ clone.sh
@@ -0,0 +1,10 @@
+git clone -l https://github.com/fcoulombe/kinevox.git 
+cd kinevox
+cd lib
+git clone -l https://github.com/fcoulombe/gcl.git
+cd ..
+cd ..
+#python Install3rdparty.py
+cd kinevox
+#GenerateVCproj.bat
+#scons @build_all @run_all 
